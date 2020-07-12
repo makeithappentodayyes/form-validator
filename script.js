3387669c251c77823173088ab6ab7d88b4ca70f3
Daniel Gil <danielgilucsd@gmail.com>
2020-07-12 04:43:50 -0700

    form control small text quried to show success or error

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+//show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+// Show success outline
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+// Event Listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    console.log(username.value);
+    if(username.value === '') {
+        showError(username, 'Username is required');
+    } else {
+        showSuccess(username);
+    }
 });
