b2ee32717081b3d1313307489458a9769f54ab02
Daniel Gil <danielgilucsd@gmail.com>
2020-07-14 01:58:10 -0700

    email validation

--- script.js
+++ script.js
@@ -11,19 +11,65 @@ function showError(input, message) {
     const small = formControl.querySelector('small');
     small.innerText = message;
 }
+
+
 // Show success outline
 
 function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
+
+// Check email is valid
+
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+   if(re.test(input.value)) {
+        showSuccess(input);
+   } else {
+       showError(input, 'Email is not valid');
+   }
+}
+
+// check required fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input){
+        console.log(input.id)
+        if(input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+        
+    })
+};
+
+// Check input Length
+
+function checkLenght(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+
+// Get fieldname
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // Event Listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    if(username.value === '') {
-        showError(username, 'Username is required');
-    } else {
-        showSuccess(username);
-    }
+
+    checkRequired([username, email, password, password2]);
+    checkLenght(username, 3, 15);
+    checkLenght(password, 6, 25);
+    checkEmail(email);
 });
